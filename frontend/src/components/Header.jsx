a08e712ae40228a81a904d52a3bf027f1b12c5db
Yunxia Zhang <onvanessano@gmail.com>
2024-08-21 01:58:52 -0700

    Create Header and Footer.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import logo from "../assets/CoziiProptech-logo_web-01.png";
 
 const Header = () => {
-  return <div>Hello</div>;
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand>
+            <img src={logo} alt="CoziiProtech" />
+          </Navbar.Brand>
+          <Navbar.Text>Welcome TO Mars</Navbar.Text>
+        </Container>
+      </Navbar>
+    </header>
+  );
 };
 
 export default Header;
