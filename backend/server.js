5099deb0514ce8c16ec85f56cf2f0e9749e1b9ce
Yunxia Zhang <onvanessano@gmail.com>
2024-08-21 17:34:20 -0700

    Initializing Backend.

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+// import dotenv from "dotenv";
+// dotenv.config();
+// import connectDB from "./config/db.js";
+// import productRoutes from "./routes/productRoutes.js";
+// import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
+// connectDB();
+
+// const port = process.env.PORT;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// app.use("/api/products", productRoutes); // everytime use the productRoutes, first hit /api/products
+
+// app.use(notFound);
+// app.use(errorHandler);
+
+app.listen(5000, () => console.log(`Server running on port ${5000}`));
