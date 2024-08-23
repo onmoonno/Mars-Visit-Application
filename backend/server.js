ee9bb4abb7918c8bbf3cdbebb33c78890f63b4e5
Yunxia Zhang <onvanessano@gmail.com>
2024-08-22 18:15:35 -0700

    deploy

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import infos from "./data/infos.js";
 import infosRoutes from "./routes/infosRoutes.js";
 import { notFound, errorHandler } from "./middlerware/errorMiddleware.js";
 
@@ -14,8 +14,24 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
+
 app.use(express.json()); // enable json data parsing
 app.use("/api/infos", infosRoutes); // everytime use the infosRoutes, first hit /api/infos
+
+const _dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static(path.join(_dirname, "/frontend/build")));
+
+  // any route that is not api will be redirected to index.html
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(_dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
